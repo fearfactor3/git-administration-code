2e8aba2fa33839bb481fab3f160e2d81d8ed92e8
Wayne <wayne.major03@gmail.com>
2022-05-07 22:42:35 -0400

    [] Changed git command.

--- shell scripts/prepare-commit-msg.sh
+++ shell scripts/prepare-commit-msg.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-MESSAGE=$(cat $1)
-TICKET=$(git rev-parse --abbrev-ref HEAD | grep -Eo '(\w+[-])?[0-9]+' | tr "[lower:]" "[:upper:]")
+FILE=$1
+MESSAGE=$(cat $FILE)
+TICKET=[$(git rev-parse --abbrev-ref HEAD | grep -Eo '^(\w+/)?(\w+[-_])?[0-9]+' | grep -Eo '(\w+[-])?[0-9]+' | tr "[:lower:]" "[:upper:]")]
 
-if [[ "$MESSAGE" == *"$TICKET"* ]]; then
-    echo "Ticket found in commit message.  Skipping..."
-    exit 0
+if [[ $TICKET == "[]" || "$MESSAGE" == "$TICKET"* ]];then
+    exit 0;
 fi
-
-echo "[$TICKET] $MESSAGE" > $1
