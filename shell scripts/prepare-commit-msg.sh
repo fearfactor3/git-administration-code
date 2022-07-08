082c42d894d1b4ee3bfc4e6a74dfe36e8418e17e
Wayne <wayne.major03@gmail.com>
2022-07-08 07:27:17 -0400

    [SRE-000 TEST-2] Testing prepare-commit-msg hook.

--- shell scripts/prepare-commit-msg.sh
+++ shell scripts/prepare-commit-msg.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-FILE=$1
-MESSAGE=$(cat $FILE)
+MESSAGE=$(cat $1)
 TICKET=[$(git rev-parse --abbrev-ref HEAD | grep -Eo '^(\w+/)?(\w+[-_])?[0-9]+' | grep -Eo '(\w+[-])?[0-9]+' | tr "[:lower:]" "[:upper:]")]
 
-if [[ $TICKET == "[]" || "$MESSAGE" == "$TICKET"* ]];then
-    exit 0;
+if [[ "$MESSAGE" == *"$TICKET"* ]]; then
+    echo "Ticket found in commit message.  Skipping..."
+    exit 0
 fi
+
+echo "$TICKET $MESSAGE" > $1 
